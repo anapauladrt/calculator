2e536b9cf8d20ff57d6ed3171d7e939a4c75977e
Ana Paula Duarte <159581509+anapauladrt@users.noreply.github.com>
2024-05-17 20:41:52 -0300

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,61 +1,61 @@
 from tkinter import* 
-variavel=Tk()
-variavel2=StringVar()
+janela=Tk()
+mudar_texto=StringVar()
 lista=[]
 def botoes(numero):
-        return Button(variavel, text=str(numero), command=lambda: novo(str(numero)), width=6,height=4, padx=10, bg="lightgreen")
+        return Button(janela, text=str(numero), command=lambda: novo(str(numero)), width=6,height=4, padx=10, bg="lightgreen")
 
 def novo(novo_texto):
         if novo_texto == "=":
-                if "/" in variavel2.get():
-                        lado = variavel2.get().split("/")
+                if "/" in mudar_texto.get():
+                        lado = mudar_texto.get().split("/")
                         if len(lado) == 2:
                                 lado1 = float(lado[0])
                                 lado2 = float(lado[1])
                                 resultado = lado1 / lado2
-                                variavel2.set(str(resultado))
+                                mudar_texto.set(str(resultado))
                         else:
-                                variavel2.set("Apenas um calculo simples por vez")
-                if "+" in variavel2.get():
-                        lado=variavel2.get().split("+")
+                                mudar_texto.set("Apenas um calculo simples por vez")
+                if "+" in mudar_texto.get():
+                        lado=mudar_texto.get().split("+")
                         if len(lado)==2:
                                 lado1=float(lado[0])
                                 lado2=float(lado[1])
                                 resultado=lado1+lado2
-                                variavel2.set(str(resultado))
+                                mudar_texto.set(str(resultado))
                         else:
-                                variavel2.set("Apenas um calculo simples por vez")
-                if "-" in variavel2.get():
-                        lado=variavel2.get().split("-")
+                                mudar_texto.set("Apenas um calculo simples por vez")
+                if "-" in mudar_texto.get():
+                        lado=mudar_texto.get().split("-")
                         if len(lado)==2:
                                 lado1=float(lado[0])
                                 lado2=float(lado[1])
                                 resultado=lado1-lado2
-                                variavel2.set(str(resultado))
+                                mudar_texto.set(str(resultado))
                         else:
-                                variavel2.set("Apenas um calculo simples por vez")
-                if "x" in variavel2.get():
-                        lado=variavel2.get().split("x")
+                                mudar_texto.set("Apenas um calculo simples por vez")
+                if "x" in mudar_texto.get():
+                        lado=mudar_texto.get().split("x")
                         if len(lado)==2:
                                 lado1=float(lado[0])
                                 lado2=float(lado[1])
                                 resultado=lado1*lado2
-                                variavel2.set(str(resultado))
+                                mudar_texto.set(str(resultado))
                         else:
-                                variavel2.set("Apenas um calculo simples por vez")    
+                                mudar_texto.set("Apenas um calculo simples por vez")    
         else:
                                 
-                if "," in variavel2.get():
-                        partes=variavel2.get().split(",")
+                if "," in mudar_texto.get():
+                        partes=mudar_texto.get().split(",")
                         if len(partes) == 2:
-                                variavel2.set(".".join(partes))
+                                mudar_texto.set(".".join(partes))
                         
                         
                 else:
-                        variavel2.set(variavel2.get() + novo_texto)
+                        mudar_texto.set(mudar_texto.get() + novo_texto)
 
 #-----------------------------------------------------------------------------------
-criar_espaco = Label(variavel, width=36, height=6, padx=10, textvariable=variavel2, relief=FLAT, bg="lightblue")
+criar_espaco = Label(janela, width=36, height=6, padx=10, textvariable=mudar_texto, relief=FLAT, bg="lightblue")
 criar_espaco.grid(row=0,column=0, columnspan=4)
 for numero in range(0,10):
         botao = botoes(numero)
@@ -70,20 +70,20 @@ botoes(7).grid(row=2, column=0)
 botoes(8).grid(row=2, column=1)
 botoes(9).grid(row=2, column=2)
 botoes(0).grid(row=5, column=1)
-ac=Button(variavel, text=str("AC"), command=lambda:variavel2.set(""), width=6,height=4, padx=10, bg="lightgreen" ).grid(row=1, column=0)
-divisao=Button(variavel,text="/", command=lambda: novo("/"), width=6,height=4, padx=10, bg="lightgreen").grid(row=1,column=2)
-multiplicacao=Button(variavel, text="x", command=lambda: novo("x"), width=6,height=4, padx=10, bg="lightgreen").grid(row=1,column=3)
-sub=Button(variavel, text="-", command=lambda: novo("-"), width=6,height=4, padx=10, bg="lightgreen").grid(row=2, column=3)
-adicao=Button(variavel, text="+", command=lambda:novo("+"), width=6,height=4, padx=10, bg="lightgreen").grid(row=3, column=3)
-igual=Button(variavel, text="=", command=lambda:novo("="), width=6,height=4, padx=10, bg="lightgreen").grid(row=4, column=3)
-virgula=Button(variavel, text=",", command=lambda:novo(","), width=6,height=4, padx=10, bg="lightgreen").grid(row=1, column=1)
-label1=Label(variavel,borderwidth=3, relief="raised", width=6,height=4, padx=10, bg="lightgreen").grid(row=5, column=0)
-label2=Label(variavel,borderwidth=3, relief="raised", width=16,height=4, padx=10, bg="lightgreen").grid(row=5, column=2, columnspan=3)
-variavel.title("calculadora")
-variavel.geometry("280x447")
-variavel.resizable(0,0)
-variavel.state("zoomed")
-variavel.state("iconic")
-variavel.iconphoto(0,PhotoImage(file="C:\\Users\\Júlia\\Desktop\\tkinter\\icone.img\\icon.png"))
-variavel['background']="lightpink"
-variavel.mainloop()
+ac=Button(janela, text=str("AC"), command=lambda:mudar_texto.set(""), width=6,height=4, padx=10, bg="lightgreen" ).grid(row=1, column=0)
+divisao=Button(janela,text="/", command=lambda: novo("/"), width=6,height=4, padx=10, bg="lightgreen").grid(row=1,column=2)
+multiplicacao=Button(janela, text="x", command=lambda: novo("x"), width=6,height=4, padx=10, bg="lightgreen").grid(row=1,column=3)
+sub=Button(janela, text="-", command=lambda: novo("-"), width=6,height=4, padx=10, bg="lightgreen").grid(row=2, column=3)
+adicao=Button(janela, text="+", command=lambda:novo("+"), width=6,height=4, padx=10, bg="lightgreen").grid(row=3, column=3)
+igual=Button(janela, text="=", command=lambda:novo("="), width=6,height=4, padx=10, bg="lightgreen").grid(row=4, column=3)
+virgula=Button(janela, text=",", command=lambda:novo(","), width=6,height=4, padx=10, bg="lightgreen").grid(row=1, column=1)
+label1=Label(janela,borderwidth=3, relief="raised", width=6,height=4, padx=10, bg="lightgreen").grid(row=5, column=0)
+label2=Label(janela,borderwidth=3, relief="raised", width=16,height=4, padx=10, bg="lightgreen").grid(row=5, column=2, columnspan=3)
+janela.title("calculadora")
+janela.geometry("280x447")
+janela.resizable(0,0)
+janela.state("zoomed")
+janela.state("iconic")
+janela.iconphoto(0,PhotoImage(file="C:\\Users\\Júlia\\Desktop\\tkinter\\icone.img\\icon.png"))
+janela['background']="lightpink"
+janela.mainloop()
